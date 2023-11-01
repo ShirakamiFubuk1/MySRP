389015289218e2007890d3d88e1740b7001f8d38
11795 <1179515821@qq.com>
2023-11-01 21:43:06 +0800

    2.1 Render camera

---
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraRenderer
+{
+    private ScriptableRenderContext context;
+
+    private Camera camera;
+    
+    public void Render(ScriptableRenderContext context,Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+    }
+}
