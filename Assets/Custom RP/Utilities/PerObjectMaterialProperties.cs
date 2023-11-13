f307d0be819e07280feaab0d5ad874531785246c
mona <1179515821@qq.com>
2023-11-13 17:46:02 +0800

    3.6 CutOff Per Object

--- Assets/Custom RP/Utilities/PerObjectMaterialProperties.cs
+++ Assets/Custom RP/Utilities/PerObjectMaterialProperties.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PerObjectMaterialProperties : MonoBehaviour
 {
     private static int baseColorId = Shader.PropertyToID("_BaseColor");
+    private static int cutOffId = Shader.PropertyToID("_CutOff");
 
     [SerializeField] private Color baseColor = Color.white;
+
+    [SerializeField, Range(0f, 1f)] private float cutOff = 0.5f;
 
     private static MaterialPropertyBlock block;
 
@@ -22,6 +25,7 @@ public class PerObjectMaterialProperties : MonoBehaviour
             block = new MaterialPropertyBlock();
         }
         block.SetColor(baseColorId,baseColor);
+        block.SetFloat(cutOffId,cutOff);
         GetComponent<Renderer>().SetPropertyBlock(block);
     }
 }
