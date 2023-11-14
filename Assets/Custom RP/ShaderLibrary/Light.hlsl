2a2767fc4c51aad141de2b25c140efd7fc66369b
mona <1179515821@qq.com>
2023-11-14 17:34:01 +0800

    2.6 Shader Loop

--- Assets/Custom RP/ShaderLibrary/Light.hlsl
+++ Assets/Custom RP/ShaderLibrary/Light.hlsl
@@ -1,6 +1,8 @@
 #ifndef CUSTOM_LIGHT_INCLUDED
 #define CUSTOM_LIGHT_INCLUDED
 
+#define MAX_DIRECTIONAL_LIGHT_COUNT 4
+
 struct Light
 {
     float3 color;
@@ -8,17 +10,25 @@ struct Light
 };
 
 CBUFFER_START(_CustomLight)
-    float3 _DirectionalLightColor;
-    float3 _DirectionalLightDirection;
+	//float4 _DirectionalLightColor;
+	//float4 _DirectionalLightDirection;
+	int _DirectionalLightCount;
+	float4 _DirectionalLightColors[MAX_DIRECTIONAL_LIGHT_COUNT];
+	float4 _DirectionalLightDirections[MAX_DIRECTIONAL_LIGHT_COUNT];
 CBUFFER_END
 
-Light GetDirectionalLight()
+Light GetDirectionalLight(int index)
 {
     Light light;
-    light.color = _DirectionalLightColor;
-    light.direction = _DirectionalLightDirection;
+    light.color = _DirectionalLightColors[index].rgb;
+    light.direction = _DirectionalLightDirections[index].xyz;
 
     return light;
+}
+
+int GetDirectionalLightCount()
+{
+    return _DirectionalLightCount;
 }
 
 #endif
--- Assets/Custom RP/ShaderLibrary/Lighting.hlsl
+++ Assets/Custom RP/ShaderLibrary/Lighting.hlsl
@@ -13,7 +13,13 @@ float3 GetLighting (Surface surface,Light light)
 
 float3 GetLighting (Surface surface)
 {
-    return GetLighting(surface,GetDirectionalLight());
+    float3 color = 0.0;
+    for(int i = 0;i<GetDirectionalLightCount();i++)
+    {
+        //叠加多个光照颜色
+        color += GetLighting(surface,GetDirectionalLight(i));
+    }
+    return color;
 }
 
 #endif
