66077b945ad69f0e47cb2018dd95113f6ffded23
mona <1179515821@qq.com>
2023-11-14 18:45:21 +0800

    3.5 Reflectivity

--- Assets/Custom RP/ShaderLibrary/BRDF.hlsl
+++ Assets/Custom RP/ShaderLibrary/BRDF.hlsl
@@ -1,6 +1,8 @@
 #ifndef CUSTOM_BRDF_INCLUDED
 #define CUSTOM_BRDF_INCLUDED
 
+#define MIN_REFLECTIVITY 0.04
+
 struct BRDF
 {
     float roughness;
@@ -8,11 +10,20 @@ struct BRDF
     float3 specular;
 };
 
+float OneMinusReflectivity(float metallic)
+{
+    float range = 1.0 - MIN_REFLECTIVITY;
+    //
+    return range - metallic * range;
+}
+
 BRDF GetBRDF(Surface surface)
 {
     BRDF brdf;
     
-    brdf.diffuse = surface.color;
+    float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
+    
+    brdf.diffuse = surface.color * oneMinusReflectivity;
     brdf.specular = 0.0;
     brdf.roughness = 1.0;
 
