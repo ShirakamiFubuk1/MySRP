814b7c6bccd7b7534a772abbf2742cf582922cf9
mona <1179515821@qq.com>
2023-11-15 15:56:41 +0800

    5.1 Custom Shader GUI

---
+++ Assets/Custom RP/Editor/CustomShaderGUI.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomShaderGUI : ShaderGUI
+{
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        base.OnGUI(materialEditor,properties);
+    }
+}
--- Assets/Custom RP/Shaders/Lit.shader
+++ Assets/Custom RP/Shaders/Lit.shader
@@ -41,4 +41,5 @@
             ENDHLSL
         }
     }
+    CustomEditor "CustomShaderGUI"
 }
