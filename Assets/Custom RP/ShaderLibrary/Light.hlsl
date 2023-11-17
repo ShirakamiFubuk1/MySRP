9a8e49fe2545d7a7abf67c4e133afc6ab0ae2659
mona <1179515821@qq.com>
2023-11-17 16:19:49 +0800

    2.5 Attenuating Light

--- Assets/Custom RP/ShaderLibrary/Light.hlsl
+++ Assets/Custom RP/ShaderLibrary/Light.hlsl
@@ -5,6 +5,7 @@
 
 struct Light
 {
+    float attenuation;
     float3 color;
     float3 direction;
 };
@@ -30,6 +31,26 @@ Light GetDirectionalLight(int index)
 int GetDirectionalLightCount()
 {
     return _DirectionalLightCount;
+}
+
+DirectionalShadowData GetDirectionalShadowData(int lightIndex)
+{
+    DirectionalShadowData data;
+    data.strength = _DirectionalLightShadowData[lightIndex].x;
+    data.tileIndex = _DirectionalLightShadowData[lightIndex].y;
+
+    return data;
+}
+
+Light GetDirectionalLight(int index,Surface surfaceWS)
+{
+    Light light;
+    light.color = _DirectionalLightColors[index].rgb;
+    light.direction = _DirectionalLightDirections[index].xyz;
+    DirectionalShadowData shadowData = GetDirectionalShadowData(index);
+    light.attenuation = GetDirectionalShadowAttenuation(shadowData,surfaceWS);
+
+    return light;
 }
 
 #endif
--- Assets/Custom RP/ShaderLibrary/Lighting.hlsl
+++ Assets/Custom RP/ShaderLibrary/Lighting.hlsl
@@ -3,7 +3,7 @@
 
 float3 IncomingLight (Surface surface,Light light)
 {
-    return saturate(dot(surface.normal,light.direction))*light.color;
+    return saturate(dot(surface.normal,light.direction)*light.attenuation)*light.color;
 }
 
 float3 GetLighting (Surface surface, BRDF brdf,Light light)
@@ -14,13 +14,13 @@ float3 GetLighting (Surface surface, BRDF brdf,Light light)
     return IncomingLight(surface,light) * DirectBRDF(surface, brdf, light);
 }
 
-float3 GetLighting (Surface surface, BRDF brdf)
+float3 GetLighting (Surface surfaceWS, BRDF brdf)
 { 
     float3 color = 0.0;
     for(int i = 0;i<GetDirectionalLightCount();i++)
     {
         //叠加多个光照颜色
-        color += GetLighting(surface,brdf,GetDirectionalLight(i));
+        color += GetLighting(surfaceWS,brdf,GetDirectionalLight(i,surfaceWS));
     }
     return color;
 }
