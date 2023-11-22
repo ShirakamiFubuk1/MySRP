2232beb6a858ec9cf475e831a9e3b1a1352e0168
mona <1179515821@qq.com>
2023-11-22 15:18:33 +0800

    3.5 Culling Shadow Sampling

--- Assets/Custom RP/ShaderLibrary/Light.hlsl
+++ Assets/Custom RP/ShaderLibrary/Light.hlsl
@@ -36,7 +36,7 @@ int GetDirectionalLightCount()
 DirectionalShadowData GetDirectionalShadowData(int lightIndex,ShadowData shadowData)
 {
     DirectionalShadowData data;
-    data.strength = _DirectionalLightShadowData[lightIndex].x;
+    data.strength = _DirectionalLightShadowData[lightIndex].x * shadowData.strength;
     data.tileIndex = _DirectionalLightShadowData[lightIndex].y + shadowData.cascadeIndex;
 
     return data;
@@ -49,7 +49,7 @@ Light GetDirectionalLight(int index,Surface surfaceWS,ShadowData shadowData)
     light.direction = _DirectionalLightDirections[index].xyz;
     DirectionalShadowData dirShadowData = GetDirectionalShadowData(index,shadowData);
     light.attenuation = GetDirectionalShadowAttenuation(dirShadowData,surfaceWS);
-    light.attenuation = shadowData.cascadeIndex * 0.25;
+    //light.attenuation = shadowData.cascadeIndex * 0.25;
 
     return light;
 }
