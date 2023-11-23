f088f4bb9a092966f1c8f8241ade3f84d1ad84b0
mona <1179515821@qq.com>
2023-11-23 17:22:45 +0800

    4.7 Blending Cascades

--- Assets/Custom RP/ShaderLibrary/Shadows.hlsl
+++ Assets/Custom RP/ShaderLibrary/Shadows.hlsl
@@ -43,6 +43,7 @@ struct ShadowData
 {
     int cascadeIndex;
     float strength;
+    float cascadeBlend;
 };
 
 float FadedShadowStrength(float distance,float scale,float fade)
@@ -53,6 +54,8 @@ float FadedShadowStrength(float distance,float scale,float fade)
 ShadowData GetShadowData(Surface surfaceWS)
 {
     ShadowData data;
+
+    data.cascadeBlend = 1.0;
     data.strength = FadedShadowStrength(
         surfaceWS.depth,_ShadowDistanceFade.x,_ShadowDistanceFade.y
         );
@@ -63,11 +66,16 @@ ShadowData GetShadowData(Surface surfaceWS)
         float distanceSpr = DistanceSquared(surfaceWS.position,sphere.xyz);
         if(distanceSpr<sphere.w)
         {
+            float fade = FadedShadowStrength(
+                distanceSpr,_CascadeData[i].x,_ShadowDistanceFade.z
+                );
             if(i==_CascadeCount-1)
             {
-                data.strength *= FadedShadowStrength(
-                    distanceSpr,_CascadeData[i].x,_ShadowDistanceFade.z)
-                ;
+                data.strength *= fade;
+            }
+            else
+            {
+                data.cascadeBlend = fade;
             }
             break;
         }
@@ -118,6 +126,17 @@ float GetDirectionalShadowAttenuation(
         float4(surfaceWS.position + normalBias, 1.0)
     ).xyz;
     float shadow = FilterDirectionalShadow(positionSTS);
+    if(global.cascadeBlend < 1.0)
+    {
+        normalBias = surfaceWS.normal *
+            (directional.normalBias * _CascadeData[global.cascadeIndex + 1].y);
+        positionSTS = mul(_DirectionalShadowMatrices[directional.tileIndex + 1],
+            float4(surfaceWS.position + normalBias,1.0)
+            ).xyz;
+        shadow = lerp(
+            FilterDirectionalShadow(positionSTS),shadow,global.cascadeBlend
+            );
+    }
     return lerp(1.0,shadow,directional.strength);
 }
 
