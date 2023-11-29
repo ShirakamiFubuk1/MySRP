6402b341fc6df259b914eef635dfba3a575801dc
mona <1179515821@qq.com>
2023-11-29 11:23:44 +0800

    2.4 Sampling the Light Map

--- Assets/Custom RP/ShaderLibrary/GI.hlsl
+++ Assets/Custom RP/ShaderLibrary/GI.hlsl
@@ -1,14 +1,19 @@
 #ifndef CUSTOM_GI_INCLUDED
 #define CUSTOM_GI_INCLUDED
 
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/EntityLighting.hlsl"
+
+TEXTURE2D(unity_Lightmap);
+SAMPLER(samplerunity_Lightmap);
+
 #if defined(LIGHTMAP_ON)
-    #define GI_ATTRIBUTE_DATA float2 lightMapUV:TEXCOORD1;
-    #define GI_VARYINGS_DATA float2 lightMapUV:VAR_LIGHT_MAP_UV;
+    #define GI_ATTRIBUTE_DATA float2 lightmapUV : TEXCOORD1;
+    #define GI_VARYINGS_DATA float2 lightmapUV : VAR_LIGHT_MAP_UV;
     //多行宏定义使用反斜杠来换行
     #define TRANSFER_GI_DATA(input,output) \
-        output.lightMapUV = input.lightMapUV * \
+        output.lightmapUV = input.lightmapUV * \
         unity_LightmapST.xy + unity_LightmapST.zw;
-    #define GI_FRAGMENT_DATA(input) input.lightMapUV
+    #define GI_FRAGMENT_DATA(input) input.lightmapUV
 #else
     #define GI_ATTRIBUTE_DATA
     #define GI_VARYINGS_DATA
@@ -21,10 +26,28 @@ struct GI
     float3 diffuse;
 };
 
-GI GetGI(float2 lightMapUV)
+float3 SampleLightmap(float2 lightmapUV)
+{
+    #if defined(LIGHTMAP_ON)
+        return SampleSingleLightmap(
+			TEXTURE2D_ARGS(unity_Lightmap, samplerunity_Lightmap), lightmapUV,
+			float4(1.0, 1.0, 0.0, 0.0),
+			#if defined(UNITY_LIGHTMAP_FULL_HDR)
+				false,
+			#else
+				true,
+			#endif
+			float4(LIGHTMAP_HDR_MULTIPLIER, LIGHTMAP_HDR_EXPONENT, 0.0, 0.0)
+            );
+    #else
+        return 0.0;
+    #endif
+}
+
+GI GetGI(float2 lightmapUV)
 {
     GI gi;
-    gi.diffuse = float3(lightMapUV,0.0);
+    gi.diffuse = SampleLightmap(lightmapUV);
 
     return gi;
 }
