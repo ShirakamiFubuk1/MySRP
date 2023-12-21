c4969e5c76d583f72a5abf7a9e0b51109318f5d2
mona <1179515821@qq.com>
2023-12-21 17:04:44 +0800

    1.7 Do not Always Apply FX

--- Assets/Custom RP/Runtime/PostFXStack.cs
+++ Assets/Custom RP/Runtime/PostFXStack.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class PostFXStack
+public partial class PostFXStack
 {
     private const string bufferName = "Post FX";
 
@@ -29,7 +29,8 @@ public class PostFXStack
     {
         this.context = context;
         this.camera = camera;
-        this.settings = settings;
+        this.settings = camera.cameraType <= CameraType.SceneView ? settings : null;
+        ApplySceneViewState();
     }
 
     public void Render(int sourceId)
