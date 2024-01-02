a9cd06db4faadf9f0ef828cfa827a9c4cc372cc7
mona <1179515821@qq.com>
2024-01-02 18:56:30 +0800

    2.1 Luma Neighborhood

--- Assets/Custom RP/Shaders/FXAAPass.hlsl
+++ Assets/Custom RP/Shaders/FXAAPass.hlsl
@@ -1,8 +1,9 @@
 #ifndef CUSTOM_FXAA_PASS_INCLUDED
 #define CUSTOM_FXAA_PASS_INCLUDED
 
-float GetLuma (float2 uv)
+float GetLuma (float2 uv, float uOffset = 0.0, float vOffset = 0.0)
 {
+    uv += float2(uOffset, vOffset) * GetSourceTexelSize().xy;
     #if defined(FXAA_ALPHA_CONTAINS_LUMA)
         return GetSource(uv).a;
     #else
@@ -10,9 +11,31 @@ float GetLuma (float2 uv)
     #endif
 }
 
+struct LumaNeighborhood
+{
+    float m, n, e, s, w;
+    float highest, lowest, range;
+};
+
+LumaNeighborhood GetLumaNeighborhood(float2 uv)
+{
+    LumaNeighborhood luma;
+    luma.m = GetLuma(uv);
+    luma.n = GetLuma(uv, 0.0, 1.0);
+    luma.e = GetLuma(uv, 1.0, 0.0);
+    luma.s = GetLuma(uv, 0.0, -1.0);
+    luma.w = GetLuma(uv, -1.0, 0.0);
+    luma.highest = max(max(max(max(luma.m, luma.n), luma.e), luma.s), luma.w);
+    luma.lowest = min(min(min(min(luma.m, luma.n), luma.e), luma.s), luma.w);
+    luma.range = luma.highest - luma.lowest;
+
+    return luma;
+}
+
 float4 FXAAPassFragment(Varyings input) : SV_TARGET
 {
-    return GetLuma(input.screenUV);
+    LumaNeighborhood luma = GetLumaNeighborhood(input.screenUV);
+    return luma.range;
 }
 
 #endif
